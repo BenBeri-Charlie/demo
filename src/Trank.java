b8a2253468eb10fe0703b265eda42f3d5d6bc754
Charlie <charliebenberi@gmail.com>
2017-09-08 23:27:10 +0300

    3

---
+++ src/Trank.java
@@ -0,0 +1,15 @@
+/**
+ * Created by charlie-richi on 14.07.17.
+ */
+
+import java.util.*;
+
+public class Trank {
+    public static void main(String[] args){
+        Heart h = new Heart();
+        h.fild();
+        Window wind = new Window();
+        Timer timer = new Timer();
+       // timer.schedule(new Engine(), 0, 4000);
+    }
+}
